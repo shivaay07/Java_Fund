733c3e1b6c6c5a7db9e311cab49e291e4b3b1ace
shivaay07 <devopsd07@gmail.com>
2021-08-01 09:40:52 -0700

    String Method

---
+++ StringMethod.java
@@ -0,0 +1,7 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String ninja = "I love java so much!";
+        int length = ninja.length();
+        System.out.println("String length is : " + length );
+    }
+}
