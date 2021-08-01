2a3f2482c7c6b86ab24d36cd61320258a1ac4300
shivaay07 <devopsd07@gmail.com>
2021-08-01 16:51:42 -0700

    fiizbuzz_assignment

---
+++ fizzbuzz_assignment_java/FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public String fizzBuzz(int num) {
+        if (num % 5 == 0 && num % 3 == 0) {
+            return "fizzbuzz";
+        }
+        else if (num % 5 == 0) {
+            return "buzz";
+        }
+        else if (num % 3 == 0) {
+            return "fizz";
+        }
+        else {
+            return String.valueOf(num);
+        }
+    }
+}
---
+++ fizzbuzz_assignment_java/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz tester = new FizzBuzz();
+        System.out.println(tester.fizzBuzz(9));
+        System.out.println(tester.fizzBuzz(10));
+        System.out.println(tester.fizzBuzz(15));
+        System.out.println(tester.fizzBuzz(16));
+    }
+}
