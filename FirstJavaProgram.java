401e98f0b68975a0ccef7864faf5b50bb1d24888
shivaay07 <devopsd07@gmail.com>
2021-07-31 19:56:05 -0700

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Shivam \n I am 30 years old \n My hometown in Hercules, CA");
+    }
+}
---
+++ HelloCoding.java
@@ -0,0 +1,5 @@
+public class HelloCoding {
+    public static void main(String[] args){
+        System.out.println("Hi Java I am coming for you");
+    }
+}
